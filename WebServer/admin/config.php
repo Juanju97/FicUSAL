acc12ce7f9aa6826faec5ea92e146970ab514d40
Enrique Hernandez <calabreshc@hotmail.com>
2018-05-04 12:48:14 +0200

    Added config.php file
    Added config.php file in order to connect with de database.

---
+++ WebServer/admin/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enriquehernandez
+ * Date: 4/5/18
+ * Time: 12:34
+ */
+
+// constantes de conexion de db
+define( 'DB_SERVER', 'localhost' );
+define( 'DB_NAME', 'administracion');
+define( 'DB_USER', 'root');
+define( 'DB_PASS', '');
+
